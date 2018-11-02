b75f4a0a87d8156b4516b6deafa611a45cd2f5f4
scorpio611 <evilmonter666@gmail.com>
2018-11-02 20:45:07 +0700

    add code variable ex

---
+++ variable/ex1.js
@@ -0,0 +1,13 @@
+var i = 10;
+var f = 20.5;
+var b = true;
+var s = "Hà Nội";
+
+document.write("i = "+i);
+document.write("<br>");
+document.write("f = "+f);
+document.write("<br>");
+document.write("b = "+b);
+document.write("<br>");
+document.write("s = "+s);
+document.write("<br>");
---
+++ variable/ex3.js
@@ -0,0 +1,9 @@
+function showmessage() {
+    var a = prompt("Nhap a",'');
+    var b = prompt("Nhap b",'');
+    if (a%b==0){
+        alert("a chia het cho b");
+    }
+    else
+        alert("a khong chia het cho b")
+}
