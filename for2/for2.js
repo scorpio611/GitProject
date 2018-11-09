5606c8cb4fce7a7280d874d2dd26e7c6d583ba89
scorpio611 <evilmonter666@gmail.com>
2018-11-09 21:23:07 +0700

    [Thực hành] Sử dụng vòng lặp lồng nhau
    [Thực hành] Sử dụng vòng lặp lồng nhau

---
+++ for2/for2.js
@@ -0,0 +1,13 @@
+var table;
+table = "<table border='1' width='300' cellspacing='0' cellpading'3'>";
+
+for(var i=1;i<=10;i++){
+    table += "<tr>";
+    for (var j=1;j<10;j++) {
+        table += "<td>" + i*j + "</td>";
+    }
+    table += "</tr>";
+
+}
+
+document.write(table);
