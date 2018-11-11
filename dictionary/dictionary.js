f1834b72a0ca46f52923ad7f6c197f33f4cea80c
scorpio611 <evilmonter666@gmail.com>
2018-11-12 01:09:37 +0700

    [Bài tập] Từ điển
    [Bài tập] Từ điển

---
+++ dictionary/dictionary.js
@@ -0,0 +1,19 @@
+
+
+
+function searchword() {
+    var english = ["dog","cat","car","board","paper","mobile","laptop"];
+    var vietnamese = ["chó","mèo","ô tô","bảng","giấy","điện thoại","máy tính xách tay"];
+    var word = document.getElementById("text").value;
+    var count=0;
+    for(var i=0;i<english.length;i++){
+        if(word===english[i]){
+            document.getElementById("result").innerText = "Từ cần tìm là "+ vietnamese[i];
+            count =1;
+        }
+        if(count!=1){
+            document.getElementById("result").innerText = "Không tìm thấy từ "+word;
+    }
+
+    }
+}
