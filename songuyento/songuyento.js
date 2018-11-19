6eb0185b65dda8f371ffe622014315fc71cc527c
scorpio611 <evilmonter666@gmail.com>
2018-11-20 00:32:35 +0700

    [Bài tập] Hiển thị các số nguyên tố đầu tiên
    [Bài tập] Hiển thị các số nguyên tố đầu tiên

---
+++ songuyento/songuyento.js
@@ -0,0 +1,26 @@
+var numbers = parseInt(prompt("Nhap so luong so nguyen to can in: "));
+var temp = "";
+var count=0;
+while (count<numbers) {
+    var N,i,grow;
+    for(N =2;N<(numbers*numbers);N++){
+         grow=0;
+         for(i=2;i+1<N;i++) {
+            if (N % i === 0 || N % (i + 1) === 0) {
+                grow++;
+                break;
+            }
+        }
+
+             if(grow===0 && N!==2){
+                 count++;
+                 temp += N+" ";
+             }
+
+
+    }
+    document.write(temp);
+}
+
+
+
